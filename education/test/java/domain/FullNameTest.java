47bfd767a1558e0fa8d3849c28aaf4728f3daa28
Jan Abe de Waal <jandewaal33@gmail.com>
2019-03-26 21:23:33 +0100

    Add tests for the FullName class

---
+++ education/test/java/domain/FullNameTest.java
@@ -0,0 +1,62 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FullNameTest {
+
+    @Test
+    public void testCreateWithPrefix() {
+        var fullName = new FullName("tom", "le", "bois");
+
+        assertNotNull(fullName);
+        assertEquals(fullName.firstName(), "tom");
+        assertEquals(fullName.prefix(), "le");
+        assertEquals(fullName.lastName(), "bois");
+    }
+
+    @Test
+    public void testCreate() {
+        var fullName = new FullName("tom", "bois");
+
+        assertNotNull(fullName);
+        assertEquals(fullName.firstName(), "tom");
+        assertEquals(fullName.prefix(), null);
+        assertEquals(fullName.lastName(), "bois");
+    }
+
+    @Test
+    public void testSetFirstNameThrowsIAE() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            // the constructor uses the setFirstName() method
+            new FullName(null, "el", "bois");
+        });
+    }
+
+    @Test
+    public void testSetPrefixThrowsIAE() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            // the constructor uses the setPrefix() method
+            new FullName("tom", null, "bois");
+        });
+    }
+
+    @Test
+    public void testSetLastNameThowsIAE() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            // the constructor uses the setLastName() method
+            new FullName("tom", "le", null);
+        });
+    }
+
+    @Test
+    public void testEquality() {
+        var fullName1 = new FullName("tom", "le", "bois");
+        var fullName2 = new FullName("bob", "sneeze");
+        var fullName3 = new FullName("tom", "le", "bois");
+
+        assertNotEquals(fullName1, fullName2);
+        assertEquals(fullName1, fullName3);
+    }
+}
